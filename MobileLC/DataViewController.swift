19ad66ed1af5c88f83d4abcfb19309665e5dd3c7
irudych18 <ihor@rudych.com>
2018-12-21 10:08:54 -0700

    added table view and chart

--- MobileLC/DataViewController.swift
+++ MobileLC/DataViewController.swift
@@ -9,12 +9,25 @@
 import Foundation
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        cell.textLabel?.text = "data"
+        cell.detailTextLabel?.text = "MY"
+        return cell
+    }
     
 }
--- MobileLC/MainViewController.swift
+++ MobileLC/MainViewController.swift
@@ -14,13 +14,25 @@ import Charts
 class MainViewController:UIViewController{
     
     @IBOutlet weak var chart: LineChartView!
-    
+    var months: [String]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let x = [0.2,10.1,14.6,17.2]
+        let y = [0.2,12.5,18.9,2.4]
+       
+        setChart(x: x, y: y)
         
     }
-    func setChart(dataPoints: [String], values: [Double]) {
+    func setChart(x: [Double], y: [Double]) {
         chart.noDataText = "You need to provide data for the chart."
+        var dataEntries:[ChartDataEntry] = []
+        for i in 0..<x.count{
+            let dataEntry = ChartDataEntry(x: x[i], y: y[i])
+            dataEntries.append(dataEntry)
+        }
         
+        let dataSet = LineChartDataSet(values: dataEntries, label: "Lines")
+        let chartData = LineChartData(dataSet: dataSet)
+        chart.data = chartData
     }
 }
